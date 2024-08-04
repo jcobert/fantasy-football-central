64893a6aa5c0a15e771229df8ac1032e64867313
Josh Cobert <jdcobert@gmail.com>
2024-08-04 12:01:39 -0400

    add dark mode, toast, and middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,25 @@
+import { getToken } from 'next-auth/jwt'
 import { NextRequest, NextResponse } from 'next/server'
 
-export function middleware(request: NextRequest) {}
+export default async function middleware(req: NextRequest) {
+  // Get the pathname of the request (e.g. /, /protected)
+  const path = req.nextUrl.pathname
+  const url = req.nextUrl.clone()
+
+  const session = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  })
+
+  // If user authenticated, redirect home page to dashboard.
+  if (path === '/') {
+    url.pathname = '/dashboard'
+    if (session) return NextResponse.redirect(url)
+    return NextResponse.next()
+  }
+
+  return NextResponse.next()
+}
 
 export const config = {
   matcher: [
